bf042eab6c3425749d60db5a25284208b287d95c
DouglasMarquesAlves <douglas.marques@55pbx.com.br>
2020-08-04 11:45:22 -0300

    .

--- app-settings.js
+++ app-settings.js
@@ -1,7 +1,7 @@
 global.database = {
 
     database_name: 'Cluster0',
-    password: 'da264248',
+    password: 'da123456789',
     user: 'admin',
     port: '27017',
     host: 'cluster0-twaan.gcp.mongodb.net',
