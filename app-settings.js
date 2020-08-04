4d9312018157f9d27bcec8aef88c00fe8a7aa40f
DouglasMarquesAlves <douglas.marques@55pbx.com.br>
2020-08-04 11:39:17 -0300

    initial commit

---
+++ app-settings.js
@@ -0,0 +1,16 @@
+global.database = {
+
+    database_name: 'Cluster0',
+    password: 'da264248',
+    user: 'admin',
+    port: '27017',
+    host: 'cluster0-twaan.gcp.mongodb.net',
+    // poolSize: 500,
+    // "replicas": {
+    //     "name": "",
+    //     "sets": [
+    //         {
+    //         }
+    //     ]
+    // }
+}
