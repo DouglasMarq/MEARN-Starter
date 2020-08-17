901a8cb39723ab429200d8692a5487df818ba8c2
DouglasMarquesAlves <douglas.marques@55pbx.com.br>
2020-08-17 14:16:17 -0300

    server js -> ts

---
+++ server/src/config/index.ts
@@ -0,0 +1,19 @@
+export = {   
+    "database": {
+        "main": {
+            "host": process.env.DB_HOST || "cluster0-twaan.gcp.mongodb.net",
+            "port": process.env.DB_PORT || "27017",
+            "name": process.env.DB_NAME || "Cluster0",
+            "user": process.env.DB_USER || "admin",
+            "pass": process.env.DB_PASS || "da123456789"
+        },
+    },
+    "server": {
+            "port": process.env.SERVER_PORT || 3000,
+            "host": process.env.SERVER_HOST || "127.0.0.1",
+            "name": process.env.SERVER_NAME || "MEAN-Starter",
+        "socket": {
+            "port": process.env.SERVER_SOCKET_PORT || 443,
+        }
+    }
+};
