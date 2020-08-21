55e46633250272e279254780f9868f426bd526de
DouglasMarquesAlves <douglas.marques@55pbx.com.br>
2020-08-21 13:49:51 -0300

    refazendo react do zero

--- client-React/src/index.js
+++ client-React/src/index.js
@@ -1,20 +1,21 @@
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import React from 'react';
-import { store, history} from './store';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import store from './app/store';
+import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
 
-import { Route, Switch } from 'react-router-dom';
-import { ConnectedRouter } from 'react-router-redux';
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
-import App from './components/App';
-
-ReactDOM.render((
-  <Provider store={store}>
-    <ConnectedRouter history={history}>
-      <Switch>
-        <Route path="/" component={App} />
-      </Switch>
-    </ConnectedRouter>
-  </Provider>
-
-), document.getElementById('root'));
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
