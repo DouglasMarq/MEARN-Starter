c6a031d95085a718edde4e2278ed7b569c8771cd
DouglasMarquesAlves <douglas.marques@55pbx.com.br>
2020-09-28 13:36:01 -0300

    Atualizando

--- client-Angular/src/app/app-routing.module.ts
+++ client-Angular/src/app/app-routing.module.ts
@@ -1,11 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './modules/general/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent },
+  {
+    path: 'about',
+    loadChildren: () => import('./modules/general/about/about.module')
+      .then(mod => mod.AboutModule)
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, {
+    initialNavigation: 'enabled'
+  })],
+  exports: [RouterModule],
+  declarations: []
 })
 export class AppRoutingModule { }
--- client-Angular/src/app/app.component.ts
+++ client-Angular/src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Renderer2 } from '@angular/core';
 
 @Component({
-  selector: 'mean-root',
+  selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   note = 'Client app is running!';
+
+  constructor(
+    public router: Router,
+    public renderer: Renderer2) { }
+
+  onSelectMenu(link: any): void {
+    const element = document.getElementById('bd-docs-nav');
+    this.renderer.removeClass(element, 'show');
+    const route = '/' + link;
+    this.router.navigate([route]);
+  }
 }
