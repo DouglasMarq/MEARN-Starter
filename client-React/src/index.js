e94c10c03c94060ccd89c96943e7af34f129ea6e
DouglasMarq <douglas.marq.alves@outlook.com>
2021-03-01 13:33:23 -0300

    Atualizações

--- client-React/src/index.js
+++ client-React/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { Store } from './store';
 import App from './App';
-import 'fontsource-roboto';
+import '@fontsource/roboto';
 
 ReactDOM.render(
   <Provider store={Store}>
